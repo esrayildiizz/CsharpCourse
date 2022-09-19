5e3ba213763107911629cf46739140f4ea0da511
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-19 09:27:41 +0300

    Döngü ile ilgili örnek eklendi.

--- Ornekler3/Program.cs
+++ Ornekler3/Program.cs
@@ -128,38 +128,237 @@ namespace Ornekler3
 
             //klavyeden girilen sayıya kadar olan tek sayıları ve çift sayıları ayrı ayrı toplayan ve tek adet çift adet sayısını veren program:
 
-            Console.WriteLine("Kaç sayı gireceksiniz: ");
-            int sayi = Convert.ToInt32(Console.ReadLine());
-            int tsayac = 0;
-            int csayac = 0;
-            int ttoplam = 0;
-            int ctoplam = 0;
-            for (int i = 0; i < sayi; i++)
-            {
-                Console.WriteLine(i + 1 + ".Sayıyı giriniz: ");
-                int  sayii= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Kaç sayı gireceksiniz: ");
+            //int adet = Convert.ToInt32(Console.ReadLine());
+            //int tsayac = 0;
+            //int csayac = 0;
+            //int ttoplam = 0;
+            //int ctoplam = 0;
+            //for (int i = 0; i < adet; i++)
+            //{
+            //    Console.WriteLine(i + 1 + ".Sayıyı giriniz: ");
+            //    int  sayii= Convert.ToInt32(Console.ReadLine());
 
-                if (sayii%2==0)
+            //    if (sayii%2==0)
+            //    {
+            //       csayac++;
+            //        ctoplam = ctoplam + sayii;
+            //    }
+            //    else
+            //    {
+            //        tsayac++;
+            //        ttoplam = ttoplam + sayii;
+            //    }
+            //}
+
+            //Console.WriteLine("ÇİFT SAYI ADEDİ: "+csayac+ "ve ÇİFT SAYILARIN TOPLAMI: "+ctoplam);
+            //Console.WriteLine("TEK SAYI ADEDİ: "+tsayac+ "ve TEK SAYILARIN TOPLAMI: " + ttoplam);
+            //Console.ReadLine();
+
+
+
+
+
+
+            //ornek7
+
+            //girilen sayının faktoriyelini alma prog.
+
+            //Console.WriteLine("Faktöriyeli alınacak sayıyı girin: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //int sonuc = 1;
+
+            //for (int i = 1; i <=sayi; i++)
+            //{
+            //    sonuc = sonuc * i;
+
+            //}
+
+            //Console.WriteLine("sonuç: "+sonuc);
+            //Console.ReadLine();
+
+
+
+
+            //ornek8
+
+            //1 den girilen sayıya kadar 7 ye tam bölünen sayıları sayan, toplamlarını hesaplayan ve bu sayıları ekrana yazdıran prog.
+
+
+            //Console.WriteLine("Lütfen sayı giriniz: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //int sayac = 0;
+            //int toplam = 0;
+            //for (int i =1 ; i < sayi; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        sayac++;
+            //        toplam = toplam + i;
+
+            //    }
+            //}
+            //Console.WriteLine("1'DEN "+sayi+" SAYISINA KADAR 7'YE BÖLÜNEN SAYILAR VE TOPLAMI: ");
+            //Console.WriteLine();
+            //Console.WriteLine("7ye böünen sayılar: " + sayac);
+            //Console.WriteLine("7ye bölünen sayıların toplamı: "+toplam);
+
+            //Console.ReadLine();
+
+
+
+
+
+
+            //ornek9
+
+            //bir komisyoncu sattığı mallardan fiyatı 50tl ye kadar olanlardan %3 , daha fazla olanlardan ise %2 komisyon almaktadır.
+            //klavyeden teker teker girilen 5 malın komisyonlarını bulup ekrana yazdıran ve en sonunda toplam komisyonu ekrana yazdıran prog.
+
+            //Console.WriteLine("***MAL FİYAT LİSTESİ***");
+            //float toplamk = 0;
+            //float komisyon1 = 0;
+            //float komisyon2 = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+
+            //    Console.WriteLine(i + 1 + ".Mal Fiyatını Giriniz: ");
+            //    float  malfiyat= Convert.ToSingle(Console.ReadLine());
+
+
+            //    if (malfiyat < 50)
+            //    {
+            //        komisyon1 = malfiyat * 3 / 100;
+            //        toplamk += komisyon1;
+            //        Console.WriteLine(" MAL FİYAT<50 İÇİN KOMİSYON TUTARI: "+komisyon1);
+
+            //    }
+            //    else
+            //    {
+            //        komisyon2 = malfiyat * 2 / 100;
+            //        toplamk += komisyon2;
+            //        Console.WriteLine(" MAL FİYAT>50 İÇİN KOMİSYON TUTARI:" + komisyon2);
+
+            //    }
+
+
+
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("TOPLAM KOMİSYON TUTARI: " + toplamk);
+
+            //Console.ReadLine();
+
+
+
+
+
+            //ornek10
+
+
+            //girilen 10 sayıyı toplayıp 5.sayıya bölen program
+
+
+            //float toplam = 0;
+            //float sonuc = 0;
+            //float bölen = 1;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i + 1 + ".Sayıyı Giriniz: ");
+            //    float sayi = Convert.ToSingle(Console.ReadLine());
+
+            //    toplam = toplam + sayi;
+
+            //    if (i == 4)
+            //    {
+            //        sayi = bölen;
+            //    }
+            //}
+
+            //sonuc = toplam / bölen;
+
+            //Console.WriteLine("Girdiğiniz sayıların toplamı: "+toplam);
+            //Console.WriteLine("Sayıların toplamının  5.sayıya bölümü: "+sonuc);
+
+
+            //Console.ReadLine();
+
+
+
+
+
+            //ornek11
+
+            //Console.WriteLine("En Büyük Ve En Küçük Sayı Bulma");
+
+            //int enbüyük = -999999;
+            //int enkücük = 9999999;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i + 1 + ".Sayıyı Giriniz: ");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //    if (sayi > enbüyük)
+            //        enbüyük = sayi;
+
+            //    else if (sayi < enkücük)
+            //        enkücük = sayi;
+            //}
+
+            //Console.WriteLine("EN BÜYÜK SAYI:  "+enbüyük);
+            //Console.WriteLine("EN KÜÇÜK SAYI: "+enkücük);
+
+            //Console.ReadLine();
+
+
+
+
+            //ornek12
+
+            //random üretilecek sayıyı kullanıdan isteyen ve girilen sayı kadar random sayı üretip iki basamaklı ve üç basamaklıların adedini gösteren
+            //diğerlerinin de 5 den küçük olanların toplamını gösteren program
+            //random değerler 1 ve 1000 arasında olucak.
+
+
+            Console.WriteLine("Kaç sayı üretilsin?");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            Random rastgele = new Random(adet);
+          
+
+            int iadet = 0;
+            int üadet = 0;
+            int toplam = 0;
+
+            for (int i = 0; i < adet; i++)
+            {
+                int sayi = rastgele.Next(0, 1000);
+                Console.WriteLine(sayi);
+                if (sayi < 100 && sayi > 9)
                 {
-                   csayac++;
-                    ctoplam = ctoplam + sayii;
+                    iadet++;
                 }
-                else
+                else if (sayi < 1000 && sayi > 99)
                 {
-                    tsayac++;
-                    ttoplam = ttoplam + sayii;
+                    üadet++;
+                }
+
+                else if (sayi < 5)
+                {
+                    toplam = toplam + sayi;
                 }
             }
+            
 
-            Console.WriteLine("ÇİFT SAYI ADEDİ: "+csayac+ "ve ÇİFT SAYILARIN TOPLAMI: "+ctoplam);
-            Console.WriteLine("TEK SAYI ADEDİ: "+tsayac+ "ve TEK SAYILARIN TOPLAMI: " + ttoplam);
+            Console.WriteLine("İki basamaklı sayıların adedi: "+iadet);
+            Console.WriteLine("Üç basamaklı sayıların adedi: "+üadet);
+            Console.WriteLine("5 den küçük sayıların toplamı: "+toplam);
+
             Console.ReadLine();
-
-
-
-
-
-
+          
+            
 
         }
     }
