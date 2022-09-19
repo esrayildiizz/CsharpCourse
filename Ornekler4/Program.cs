a97af77e650646dc9de5f2d27d35a58158dd4fd0
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-19 12:40:46 +0300

    Yildizlarla sekil cizme ornegi eklendi.

---
+++ Ornekler4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornekler4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Yıldızlarla şekil çizme
+            //ornek1
+            Console.WriteLine("BİR SAYI GİRİN: ");
+            int sayi= Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <=sayi; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
