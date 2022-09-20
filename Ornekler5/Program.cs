9e6a6a3c9c63de27bdb5a871f1bba3860a7dbffa
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-20 10:35:52 +0300

    Faktoriyel ornegi eklendi.

--- Ornekler5/Program.cs
+++ Ornekler5/Program.cs
@@ -41,7 +41,7 @@ namespace Ornekler5
             //    toplam = toplam + sayi;
             //}
 
-           
+
             //ortalama = toplam / 10;
 
             //Console.WriteLine("1 den 10 a kadar sayıların toplam: " + toplam);
@@ -64,7 +64,7 @@ namespace Ornekler5
             //{
             //    if (sayi % 2 == 1)
             //    {
-                   
+
             //        Console.WriteLine("Tek sayılarımız: "+sayi);
             //        carpim = carpim * sayi;
 
@@ -75,6 +75,86 @@ namespace Ornekler5
 
             //Console.WriteLine("1 den 100 e kadar tek sayıların çarpımı: "+carpim);
             //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+            //ornek4
+            //kira hesabını yapan prog.
+
+            //int yil = 2005;
+            //Console.WriteLine("Kira tutarınızı giriniz: ");
+            //int kira = Convert.ToInt32(Console.ReadLine());
+            //while (yil < 2015)
+            //{
+            //    yil++;
+            //    kira = kira+(kira * 25 / 100);
+
+            //    Console.WriteLine("yıl: "+yil +"  kira tutarı: "+kira);
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+
+
+            //ornek5
+
+            //Console.WriteLine("Lütfen sayı giriniz");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //int sayac = 0;
+            //while (sayi %10>0)
+            //{
+            //    sayi = sayi / 10;
+            //    sayac++;
+            //}
+            //Console.WriteLine("Girdiğiniz sayı "+sayac+" basamaklıdır.");
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+            //ornek6 
+
+            //int sayi;
+            //Console.WriteLine("Faktöriyeli alınacak sayıyı girin: ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+
+            //int faktoriyel = 1;
+
+            //while (sayi >=1 )
+            //{
+            //    faktoriyel = faktoriyel * sayi;
+            //    sayi--;
+            //}
+
+            //Console.WriteLine("SONUÇ: "+faktoriyel);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
