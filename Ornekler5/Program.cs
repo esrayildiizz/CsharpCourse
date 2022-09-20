8c4d585b5ffe70eb1bb80ce5c37d69c4f26d3e21
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-20 09:33:09 +0300

    while dongüsü orneği eklendi.

---
+++ Ornekler5/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornekler5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While döngüsü
+            //ornek1
+
+            //int sayi = 0;
+
+            //while (sayi < 10)
+            //{
+            //    sayi++;
+            //    Console.WriteLine("sayi: "+sayi);
+            //}
+
+            //Console.WriteLine("Döngü bitti");
+
+            //Console.ReadLine();
+
+
+
+
+            //ornek2
+
+            //float sayi = 0;
+            //float toplam = 0;
+            //float ortalama = 0;
+
+            //while (sayi < 10)
+            //{
+            //    sayi++;
+            //    Console.WriteLine("sayi: " + sayi);
+            //    toplam = toplam + sayi;
+            //}
+
+           
+            //ortalama = toplam / 10;
+
+            //Console.WriteLine("1 den 10 a kadar sayıların toplam: " + toplam);
+            //Console.WriteLine("1 den 10 a kadar sayıların ortalaması: " + ortalama);
+
+            //Console.ReadLine();
+
+
+
+
+
+
+            //ornek3
+
+            // 0 ile 100 arasındaki tek sayılarının çarpımını ve tek sayıları gösteren prog.
+
+            //double sayi = 0;
+            //double carpim = 1;
+            //while (sayi < 100)
+            //{
+            //    if (sayi % 2 == 1)
+            //    {
+                   
+            //        Console.WriteLine("Tek sayılarımız: "+sayi);
+            //        carpim = carpim * sayi;
+
+            //    }
+
+            //    sayi++;
+            //}
+
+            //Console.WriteLine("1 den 100 e kadar tek sayıların çarpımı: "+carpim);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
