2625ad0e70506c61053226c624fd011600d648c8
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-21 10:30:28 +0300

    Foreach dongusu ornegi eklendi.

---
+++ Ornekler6/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornekler6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //foreach döngüsü
+            //foreach listeler ya da diziler üzerinde işlem yapmak için kullanılan döngü yapısıdır.
+            //Özellikle eleman sayısının bilinmediği durumlarda büyük kolaylık sağlamaktadır.
+
+
+            //ornek1
+
+            //string harf = "abcfff";
+            //string rakam = "01234";
+
+            //foreach(char a in harf) //harfin içindeki elemanları tek tek getir.
+            //{
+            //    Console.WriteLine("Karakter Değeri:"+a);
+            //}
+            //foreach (char b in rakam)
+            //{
+            //    Console.WriteLine("Karakter Değeri:"+b);
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+            //ornek2
+
+            //string sesliharf = "aeıioöuü";
+            //int sayac = 0;
+            //Console.WriteLine("Metin giriniz: ");
+            //string metin = Convert.ToString(Console.ReadLine());
+            //foreach (char item in metin)
+            //{
+            //    foreach (char item2 in sesliharf)
+            //    {
+            //        if (item == item2)
+            //        {
+            //            sayac++;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("Metin Uzunluğu: "+metin.Length);
+            //Console.WriteLine("Metin İçindeki Sesli Harf Sayısı: "+sayac);
+
+
+            //Console.ReadLine();
+
+
+
+            //ornek3
+
+
+            //string sesliharf = "aeıioöuü";
+            //string sessizharf = "bcçdfgğhjklmnprsştvyz";
+            //string rakam = "0123456789";
+            //string karakter = "+-*/";
+
+            //int sayac = 0;
+            //int sayac1 = 0;
+            //int sayac2 = 0;
+            //int sayac3 = 0;
+
+            //Console.WriteLine("Metin giriniz: ");
+            //string metin = Convert.ToString(Console.ReadLine());
+            //foreach (char item in metin)
+            //{
+            //    foreach (char item2 in sesliharf)
+            //    {
+            //        if (item == item2)
+            //        {
+            //            sayac++;
+            //        }
+                    
+            //    }
+            //    foreach (char item3 in sessizharf)
+            //    {
+            //        if (item == item3)
+            //        {
+            //            sayac1++;
+            //        }
+
+            //    }
+            //    foreach (char item4 in rakam)
+            //    {
+            //        if (item == item4)
+            //        {
+            //            sayac2++;
+            //        }
+
+            //    }
+            //    foreach (char item5 in karakter)
+            //    {
+            //        if (item == item5)
+            //        {
+            //            sayac3++;
+            //        }
+
+            //    }
+            //}
+
+            //Console.WriteLine("Metin Uzunluğu: "+metin.Length);
+            //Console.WriteLine("Metin İçindeki Sesli Harf Sayısı: "+sayac);
+            //Console.WriteLine("Metin İçindeki Sessizli Harf Sayısı: "+sayac1);
+            //Console.WriteLine("Metin İçindeki Rakam Sayısı: "+sayac2);
+            //Console.WriteLine("Metin İçindeki Karakter Sayısı: "+sayac3);
+
+
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
