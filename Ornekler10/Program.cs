07b62685a2ef9763d95390d89aff47d5b06f2260
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-23 14:49:44 +0300

    Detayli metot ornegi eklendi.

--- Ornekler10/Program.cs
+++ Ornekler10/Program.cs
@@ -35,6 +35,61 @@ namespace Ornekler10
            
         }
 
+        public static void Secim2()
+        {
+            Console.WriteLine("****Orta Oda Seçenekleri****");
+            Console.WriteLine("a-2 Kişilik Odalar");
+            Console.WriteLine("b-2 Den Fazla Kişilik Odalar");
+            char secim = Convert.ToChar(Console.ReadLine());
+            if (secim == 'a')
+            {
+                Console.WriteLine("Odanın günlük fiyatını girin");
+                int fiyat= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Yemek günde kaç öğün olsun?");
+                int ydurum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Oda servisi olsun mu?");
+                string odaservis = Console.ReadLine();
+                Console.WriteLine("Yemek parası ne kadar?");
+                int yparasi= Convert.ToInt32(Console.ReadLine());
+                if (ydurum==3 &&( odaservis == "Evet" || odaservis == "evet"))
+                {
+                    int sonuc = fiyat + (3 * (yparasi)) + (fiyat * 7 / 100);
+                    Console.WriteLine("Ödeyeceğiniz tutar: "+sonuc);
+                }
+                else if(ydurum==2 && odaservis == "Evet")
+                {
+                    int sonuc = fiyat + (2 * (yparasi)) + (fiyat * 2 / 100);
+                    Console.WriteLine("Ödeyeceğiniz tutar: " + sonuc);
+                }
+                
+            }
+            else if (secim == 'b')
+            {
+                int ofiyat = 500;
+                int yemek = 50;
+                Console.WriteLine("Kaç kişi kalacaksınız?");
+                int kisi= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Çocuk var mı?");
+                string cvp= Console.ReadLine();
+                if(cvp=="Evet" || cvp == "evet")
+                {
+                    Console.WriteLine("çocuk yaşı kaç?");
+                    int cocuk = Convert.ToInt32(Console.ReadLine());
+                    if (cocuk < 7)
+                    {
+                        Console.WriteLine(" 7 yaşından küçük çocuk için ücret alınmayacaktır.");
+                    }
+                    else 
+                    {
+                        int hesapla = (ofiyat + (3 * (yemek))) * kisi + (ofiyat * 3 / 100);
+                        Console.WriteLine("Ödemeniz gereken tutar: " + hesapla);
+                    }
+                }
+               
+            }
+
+        }
+
         public static bool Git(string cevap)
         {
             if (cevap == "Evet" || cevap == "evet")
@@ -63,7 +118,7 @@ namespace Ornekler10
 
                 case 2:
                     Console.WriteLine("***Orta Odalar***");
-
+                    Secim2();
                     break;
 
                 case 3:
