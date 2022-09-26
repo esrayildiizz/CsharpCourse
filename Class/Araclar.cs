563feb1186e8ed7ee2c809824ae59e4cb6ef3cb4
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-26 14:47:22 +0300

    Class aciklamasi yapildi.

---
+++ Class/Araclar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Araclar
+    {
+        public string marka;
+        public string model;
+        public double fiyat;
+        public double vergiorani;
+
+        public double OTV(double fiyat,double vergiorani)
+        {
+            fiyat = fiyat * vergiorani / 1000;
+
+            fiyat *= 2;
+            return fiyat;
+        }
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Araclar arac1 = new Araclar();
+
+            arac1.marka = "hyundai";
+            arac1.model = "i35";
+            arac1.fiyat = 400000;
+            arac1.vergiorani = 12.5d;
+            double tutar = arac1.OTV(arac1.fiyat, arac1.vergiorani);
+            Console.WriteLine("Araba fiyatı: "+tutar);
+
+            Console.ReadLine();
+        }
+    }
+}
