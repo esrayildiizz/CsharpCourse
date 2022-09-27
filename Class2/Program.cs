092e4badfe84803eb5b9ee27200679d1076add62
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-27 12:22:16 +0300

    Class ornekleri eklendi.

---
+++ Class2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urunler urun1 = new Urunler();
+
+            Console.WriteLine("Ürün nosu giriniz");
+            urun1.UrunNo = Console.ReadLine();
+
+            Console.WriteLine("Marka giriniz");
+            urun1.Marka= Console.ReadLine();
+
+            Console.WriteLine("Ürün numarası: "+urun1.UrunNo);
+            Console.WriteLine("Ürün markası: "+urun1.Marka);
+
+
+            double a = urun1.Tutar(1000, 18);
+            Console.WriteLine("Tutar: "+a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class2/Urunler.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    class Urunler
+    {
+        private string urunno;
+        private string marka;
+        private string model;
+        private int fiyat;
+        private string cekdurumu;
+        private double kdvorani;
+
+
+        public string UrunNo
+        {
+            get
+            { 
+                return urunno;
+            }
+            set 
+            {
+                urunno = value; 
+            }
+        }
+
+        public string Marka
+        {
+            get 
+            {
+                return marka; 
+            }
+            set 
+            { 
+                if(value=="vestel" || value == "beko")
+                {
+                    marka = value;
+                }
+                else
+                {
+                    Console.WriteLine("Başka firmalarla çalışmıyooruz....");
+                }
+            }
+        }
+
+        public string Model
+        {
+
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+       public int Fiyat
+        {
+            get
+            {
+                return fiyat;
+            }
+            set
+            {
+                if (value>10000 && value<20000 )
+                {
+                    fiyat = value;
+                }
+                else
+                {
+                    Console.WriteLine("Fiyat aralığımıza uygun değil...");
+                }
+            }
+
+        }
+
+        public string CekDurumu
+        {
+            get
+            {
+                return cekdurumu;
+            }
+            set
+            {
+                cekdurumu = value;
+            }
+        }
+
+        public double KdvOrani
+        {
+            get
+            {
+                return kdvorani;
+            }
+            set
+            {
+                if (value >1 && value < 18)
+                {
+                   kdvorani = value;
+                }
+                else
+                {
+                    Console.WriteLine("Yüzde aralığımıza uygun değil...");
+                }
+            }
+        }
+
+
+        public double Tutar(double fiyat,double kdvorani)
+        {
+            fiyat += fiyat * kdvorani / 100;
+            return fiyat;
+        }
+
+
+    }
+}
