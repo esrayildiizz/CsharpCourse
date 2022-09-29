5c7977ad2a8a87d032ce0218ae04d91887921f68
Esra Yıldız <esrayildiizz@outlook.com>
2022-09-29 14:30:14 +0300

    Düzenlendi.

---
+++ Kalıtım4/Motorsiklet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalıtım4
+{
+    class Motorsiklet
+    {
+        public string ekipmanlar;
+        public int depo;
+    }
+}
---
+++ Kalıtım4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalıtım4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Motorlutasitlar sınıfından nesne üretemeyiz.
+            //soyut class yaptık ve bu classtan nesne üretemeyiz.
+            //nesne üretemeyince içindeki field ve propertylere ulaşamayız. o yüzden kalıtım bırakarak erişim sağlarız.
+            //bunun tam tersi sealed class.tır.
+
+
+            Motorsiklet m1 = new Motorsiklet();
+            
+        }
+    }
+}
