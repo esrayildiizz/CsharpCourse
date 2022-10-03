a267fc9f90a3c702f1072f67b0996e040de07527
Esra Yıldız <esrayildiizz@outlook.com>
2022-10-03 14:50:08 +0300

    duzenleme yapildi.

---
+++ Ornek4/BeyazEsya.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek4
+{
+    class BeyazEsya
+    {
+        private int esyano;
+        private string esyaadi;
+        private int fiyat;
+
+        //public BeyazEsya() //constructor metot tanımladık //void gibidir.//class isminde olmalı.
+        //{
+        //    esyano = 555;
+        //    esyaadi = "robot süpürge";
+        //    fiyat = 2500;
+        //}
+        public BeyazEsya(int esyano,string esyaadi,int fiyat) //parametreli
+        {
+            this.esyano = esyano;
+            this.esyaadi = esyaadi;
+            this.fiyat = fiyat;
+        }
+
+        public int EsyaNo
+        {
+            get
+            {
+                return esyano;
+            }
+            set
+            {
+                if (value == 456)
+                {
+                    esyano = value;
+                }
+                else
+                {
+                    Console.WriteLine("Girilen değer olmadı...");
+                }
+            }
+        }
+
+        public string EsyaAdi
+        {
+            get
+            {
+                return esyaadi;
+            }
+            set
+            {
+                if (value == "buzdolabı")
+                {
+                    esyaadi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Başka ürünüm yok varsayılan ürünü almak zorundasınız.");
+                }
+            }
+        }
+
+        public int Fiyat
+        {
+            get
+            {
+                return fiyat;
+            }
+            set
+            {
+                if (value == 5000)
+                {
+                    fiyat = value;
+                }
+                else
+                {
+                    Console.WriteLine("Başka fiyat yok....");
+                }
+            }
+        }
+    }
+}
---
+++ Ornek4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek4
+{
+    class Program
+    {
+        //constructor metot
+        //nesneye ilk değer atamaları yapabilmek.
+        //C# Constructor tanımlaması yapacağımız zaman, yapıcı metodumuzun ismi ile sınıfımızın ismi aynı olmak zorundadır.
+        //Constructor(Yapıcı / Kurucu) metotlarımız, geriye herhangi bir değer döndüremediklerinden dolayı void yapıda tanımlanmalıdır.
+        //Tanımlamayı yaparken erişim belirleyicisini mutlaka public olarak belirtmemiz gerekmektedir.Aksi takdirde sınıf yapımıza erişemeyeceğimiz gibi,
+        //daha önce tanımladığımız yerlerde hata ile karşılaşırız.
+
+        static void Main(string[] args)
+        {
+            BeyazEsya urun1 = new BeyazEsya(444,"ütü",4750);
+            urun1.EsyaNo = 789;
+            urun1.EsyaAdi = "çamaşır makinesi";
+            urun1.Fiyat = 7800;
+
+            Console.WriteLine("Urun no: "+urun1.EsyaNo);
+            Console.WriteLine("Urun adı: "+urun1.EsyaAdi);
+            Console.WriteLine("Urun fiyat: "+urun1.Fiyat);
+
+
+            Console.ReadLine();
+        }
+    }
+}
