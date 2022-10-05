2ea2dd157aa52930496edd6ef3713f55138b8d9e
Esra Yıldız <esrayildiizz@outlook.com>
2022-10-05 14:42:30 +0300

    static class ornegi eklendi.

---
+++ Ornek6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek6
+{
+    class Program
+    { 
+      
+        static void Main(string[] args)
+        {
+
+            //Calışanlardan nesne üretmeden oradaki metotları çağırabiliyoruz..
+            calisanlar.Getir();
+            calisanlar.Hesapla();
+
+
+            //Mesela aşağıdaki console.Readline için şöyle bir açıklama yapabiliriz.
+            //Console sınıfı static olarak tanımlandığı için Readline METODU nesne üretmemize gerek kalmıyor direk erişim sağlıyoruz.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ornek6/calisanlar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek6
+{
+      static class calisanlar
+      {
+        //bir class static ise içindekiler static olmalıdır.
+        //bir alanı static tanımladıysak ondan nesne üretemeyiz.
+        //bir alana otomatik olarak ulaşmak istiyorsam orayı static olarak tanımlarım.
+
+        public static int calisanno;
+        public static string calisanadi;
+        public static int maas;
+        public static string adres;
+
+
+        public static void Hesapla()
+        {
+            Console.WriteLine("Paraları getir....");
+        }
+        public static void Getir()
+        {
+            Console.WriteLine("Maaşlar ayın 10 undan sonra yatıcak inş....");
+        }
+      }
+}
