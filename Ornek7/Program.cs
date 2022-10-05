e623e1a139a1846dd4e729219c2ed8ea1eee4db8
Esra Yıldız <esrayildiizz@outlook.com>
2022-10-05 14:42:56 +0300

    struct ile ilgili ornek yapildi.

---
+++ Ornek7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek7
+{
+    class Program
+    {
+
+        struct Kolej
+        {
+            public int kolejno;
+            private string okuladi;
+
+            public string Okuladi
+            {
+                get
+                {
+                    return okuladi;
+                }
+                set
+                {
+                    okuladi = value;
+                }
+            }
+
+            public void VeliBilgilendirme()
+            {
+                Console.WriteLine("Haftasonu hilton otelde seminer var gelmenizi rica ederiz. ");
+            }
+        } 
+
+        static void Main(string[] args)
+        {
+            Kolej aa = new Kolej();
+            aa.kolejno = 14;
+            aa.Okuladi = "ATATÜRK OKULU";
+            aa.VeliBilgilendirme();
+
+            okul okul1 = new okul();
+            okul.Kolej1 gg = new okul.Kolej1();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ornek7/okul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek7
+{
+    class okul
+    {
+        
+        public struct Kolej1
+        {
+            public int kolejno;
+            private string okuladi;
+
+            public string Okuladi
+            {
+                get
+                {
+                    return okuladi;
+                }
+                set
+                {
+                    okuladi = value;
+                }
+            }
+
+            public void VeliBilgilendirme()
+            {
+                Console.WriteLine("Haftasonu hilton otelde seminer var gelmenizi rica ederiz. ");
+            }
+        }
+    }
+}
