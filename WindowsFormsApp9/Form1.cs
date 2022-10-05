c35b08ef1d683779e73f903ea41994b2164b0abe
Esra Yıldız <esrayildiizz@outlook.com>
2022-10-05 13:17:00 +0300

    Siparis verme ekrani yapildi.

---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 go = new Form2();
+            go.Show();
+            this.Hide();
+            go.listBox1.Items.Add("Müşterinin Adı: " + textBox1.Text);
+            go.listBox1.Items.Add("Müşterinin Soyadı: " + textBox2.Text);
+            go.listBox1.Items.Add("Ödeme Şekli: " +comboBox1.SelectedItem);
+            go.listBox1.Items.Add("Sipariş no: " + textBox3.Text);
+            go.listBox1.Items.Add("Telefon: " + maskedTextBox1.Text);
+            go.listBox1.Items.Add("Adres: " + textBox4.Text);
+
+            go.listBox2.Items.Add("Yiyecek: " + comboBox2.SelectedItem);
+            go.listBox2.Items.Add("İçecek: " + comboBox3.SelectedItem);
+
+            if (checkBox1.Checked == true )
+            {   
+               go.listBox2.Items.Add("Lahmacun Seçimi: " + checkBox1.Text);
+               int lahmacunfiyat = Convert.ToInt32(textBox5.Text);
+               int lahmacunadet = Convert.ToInt32(textBox8.Text);
+               int tutar1 = lahmacunadet * lahmacunfiyat;
+               go.listBox2.Items.Add("Lahmacun için ödemeniz gereken tutar: " + tutar1 + " -İçecek ikramımızdır.");
+            }
+            else if (checkBox2.Checked == true)
+            {
+                go.listBox2.Items.Add("Lahmacun Seçimi: " + checkBox2.Text);
+                int lahmacunfiyat = Convert.ToInt32(textBox5.Text);
+                int lahmacunadet = Convert.ToInt32(textBox8.Text);
+                int tutar1 = lahmacunadet * lahmacunfiyat;
+                go.listBox2.Items.Add("Lahmacun için ödemeniz gereken tutar: " + tutar1 + " -İçecek ikramımızdır.");
+            }
+            else if (checkBox3.Checked == true)
+            {
+              go.listBox2.Items.Add("Pizza Seçimi: " + checkBox3.Text);
+              int pizzafiyat = Convert.ToInt32(textBox6.Text);
+              int pizzaadet = Convert.ToInt32(textBox9.Text);
+              int tutar2 = pizzaadet * pizzafiyat;
+              go.listBox2.Items.Add("Pizza için ödemeniz gereken tutar: " + tutar2 + " -İçecek ikramımızdır.");
+            }
+            else if (checkBox4.Checked == true)
+            {
+                go.listBox2.Items.Add("Pizza Seçimi: " + checkBox4.Text);
+                int pizzafiyat = Convert.ToInt32(textBox6.Text);
+                int pizzaadet = Convert.ToInt32(textBox9.Text);
+                int tutar2 = pizzaadet * pizzafiyat;
+                go.listBox2.Items.Add("Pizza için ödemeniz gereken tutar: " + tutar2 + " -İçecek ikramımızdır.");
+            }
+            else if (checkBox6.Checked == true  )
+            {
+               go.listBox2.Items.Add("Kebap Seçimi: " + checkBox6.Text);
+               int kebapfiyat = Convert.ToInt32(textBox7.Text);
+               int kebapadet = Convert.ToInt32(textBox10.Text);
+               int tutar3 = kebapadet * kebapfiyat;
+               go.listBox2.Items.Add("Kebap için ödemeniz gereken tutar: " + tutar3 + " -İçecek ikramımızdır.");
+            }
+            else if(checkBox7.Checked == true)
+            {
+                go.listBox2.Items.Add("Kebap Seçimi: " + checkBox7.Text);
+                int kebapfiyat = Convert.ToInt32(textBox7.Text);
+                int kebapadet = Convert.ToInt32(textBox10.Text);
+                int tutar3 = kebapadet * kebapfiyat;
+                go.listBox2.Items.Add("Kebap için ödemeniz gereken tutar: " + tutar3 + " -İçecek ikramımızdır.");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox2.Visible = false;
+            groupBox3.Visible = false;
+            groupBox4.Visible = false;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (comboBox2.SelectedItem.ToString() == "Lahmacun")
+            {
+                groupBox2.Visible = true;
+                groupBox3.Visible = false;
+                groupBox4.Visible = false;
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Pizza")
+            {
+                groupBox2.Visible = false;
+                groupBox3.Visible = true;
+                groupBox4.Visible = false;
+            }
+            else
+            {
+                groupBox2.Visible = false;
+                groupBox3.Visible = false;
+                groupBox4.Visible = true;
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp9/Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace WindowsFormsApp9
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(104, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 173);
+            this.listBox1.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(101, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Müşteri Bilgileri:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(328, 124);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(399, 173);
+            this.listBox2.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(325, 97);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 15);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Yiyecek Bilgileri:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form2";
+            this.Text = "Müşteri Bilgileri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.ListBox listBox2;
+    }
+}
