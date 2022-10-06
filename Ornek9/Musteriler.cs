bda0ac25bb627ecf00b47863c684fc3dc0cd6fa2
Esra Yıldız <esrayildiizz@outlook.com>
2022-10-06 09:32:43 +0300

    Nested class ornegi eklendi.

---
+++ Ornek9/Musteriler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek9
+{
+    //iç içe classlar (nested class-gömülü class)
+    public class Musteriler  //outer class ın erişim belirleyicisi internal
+    {
+        public int musterino;
+        public string musteriadsoyad;
+
+        public void Bilgi()
+        {
+            Console.WriteLine("Standart Müşteri");
+        }
+
+        public class VipMusteriler: Musteriler //inner class ın erişim belirleyicisi private
+        {
+            public int bakiye;
+            public string uye;
+
+            public void Durum()
+            {
+                Console.WriteLine("Zengin Müşteri");
+            }
+        }
+    }
+}
---
+++ Ornek9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteriler.VipMusteriler musteri1 = new Musteriler.VipMusteriler();
+            musteri1.bakiye = 8000;
+            musteri1.uye = "evet";
+            musteri1.musteriadsoyad = "ali";
+            musteri1.Durum();
+
+            Musteriler musteri2 = new Musteriler();
+            musteri2.musteriadsoyad = "Ali";
+            musteri2.musterino = 12;
+
+            musteri2.Bilgi();
+
+
+            KralMusteri musteri3 = new KralMusteri();
+            musteri3.musteriadsoyad = "veli";
+            musteri3.musterino = 45;
+
+        }
+    }
+}
