2ad2a984d627c1fe2774343cf5aed1158bab7828
Esra Yıldız <esrayildiizz@outlook.com>
2022-10-06 09:14:13 +0300

    Static aciklandi.

---
+++ Ornek8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sporcular.adsoyad = "enes";
+            sporcular.sporcuno = 15;
+            sporcular.SporcuBrans();
+            sporcular.Bolge();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ornek8/sporcular.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek8
+{
+    static class sporcular
+    {
+        public static int sporcuno;
+        public static string adsoyad;
+
+        public static void SporcuBrans()
+        {
+            Console.WriteLine("Boks ve güreş dalları vardır.");
+        }
+
+        public static void Bolge()
+        {
+            Console.WriteLine("Sporcular Ataşehirde iyi para kaxzanır.");
+        }
+    }
+}
