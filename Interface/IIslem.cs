42d8a9f2685fa39a50161d18cbd74aea9980c7f2
Esra Yıldız <esrayildiizz@outlook.com>
2022-10-21 09:19:19 +0300

    Interface hakkinda bilgi verildi.

---
+++ Interface/IIslem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal interface IIslem
+    {
+        int fiyat { get; set; } 
+
+        void Hesaplama(int fiyat);
+
+        int Vergi(int fiyat);
+
+        void Ekdurumlar();
+    }
+}
---
+++ Interface/Urunler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Urunler : IIslem
+    {
+        public int fiyat { get ; set ; }
+
+        public void Ekdurumlar()
+        {
+            Console.WriteLine("Ürünlerimizde zarardan biz sorumlu değiliz...");
+            Console.WriteLine("Garanti süresi iki yıldır.");
+        }
+
+        public void Hesaplama(int fiyat)
+        {
+            int sontutar=Vergi(fiyat);
+            sontutar += sontutar * 30 / 100;
+            Console.WriteLine("Ödemeniz gereken tutar: "+ sontutar);
+        }
+
+        public int Vergi(int fiyat)
+        {
+            fiyat += fiyat * 18 / 100;
+            return fiyat;   
+        }
+    }
+}
